d36f92e7cf6b60d4145d4fa92a041c6b14a28a95
AtilaSaraiva <atilasaraiva@gmail.com>
2021-04-06 13:00:34 -0300

    Managed train the network by overfitting one pair of migrated and remigrated images, and applied it to the migrated image. It still only has positive values, so I will look into that for now

--- modelCreationLSM.py
+++ modelCreationLSM.py
@@ -6,25 +6,43 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from unetmodel import unet
 from readRSF import *
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from pickle import dumps, loads
 
 block = (64,64)
 
 # Reading the migrated image ( m1 = Ltd )
 file_migratedImg = "madagascarBuild/rtmlap.rsf"
 migratedImg = read_rsf(file_migratedImg)
+migratedImg = migratedImg[30:286,30:286]
 migratedImgReshape = cropArraytoDataset(block, migratedImg)
 
 # Reading the remigrated image ( m2 = LtLm )
 file_remigratedImg = "madagascarBuild/rtmMigModlap.rsf"
 remigratedImg = read_rsf(file_remigratedImg)
+remigratedImg = remigratedImg[30:286,30:286]
 remigratedImgReshape = cropArraytoDataset(block, remigratedImg)
 
-model = unet(input_size = (*block,1))
+# model = unet(input_size = (*block,1))
+# dataset = (remigratedImgReshape, migratedImgReshape)
 
-dataset = (remigratedImgReshape, migratedImgReshape)
-# dataset = (remigratedImg, migratedImg)
-model.fit(*dataset, validation_split = 0.25, epochs=10, batch_size=10)
+# scaleModel = StandardScaler()
+scaleModel = RobustScaler()
+scaleModel.fit(remigratedImg)
+norm_remigratedImg = scaleModel.transform(remigratedImg)
+# print("mean:\n",scaleModel.mean_)
+# print("var:\n",scaleModel.var_)
+
+inputShape = (256,256)
+
+norm_remigratedImg = norm_remigratedImg.reshape(1,*inputShape,1)
+migratedImg        = migratedImg.reshape(1,*inputShape,1)
+
+dataset = (norm_remigratedImg, migratedImg)
+
+
+model = unet(input_size = (*inputShape,1))
+model.fit(*dataset, epochs=1000)
 
 # serialize weights to HDF5
 model.save_weights("unet_weights.h5")
@@ -34,7 +52,11 @@ print("Saved model weights to disk.")
 model.save("unet.h5")
 print("Saved model to disk.")
 
+with open("scaleModel.bin","wb+") as arq:
+    objectBinaryDump = dumps(scaleModel)
+    arq.write(objectBinaryDump)
 
-plt.imshow(migratedImg[30:286,30:286])
-plt.colorbar()
-plt.show()
+
+# plt.imshow(dataset[0])
+# plt.colorbar()
+# plt.show()
